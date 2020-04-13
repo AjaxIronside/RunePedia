e68e911f9b4c6788e7728acb452b729ec80735c2
Al Such <50285145+AjaxIronside@users.noreply.github.com>
2020-04-13 11:29:06 -0400

    Cleaning up the repo.
    I forgot to include the source to this update, but at least now everything's in handy little folders.

---
+++ Source/RPTest2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPTest2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            webBrowser1.Url = new Uri(String.Format("file:///{0}/index.html", curDir));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
